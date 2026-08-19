ee8a3d3b3b31023e1bf2f1505c5c524a7606cd2a
Furqan Mehdi <furqanmehdi831@gmail.com>
2026-08-19 23:57:14 +0500

    Registration Locators Added

---
+++ Locators/userRegisterLocator.js
@@ -0,0 +1,9 @@
+export const userRegisterLocator = {
+    signuplink: 'a[href="/login"]',
+    pageInfo: 'div{class="signup-form"}',
+    nameInput: 'input[name="name"]',
+    emailInput: 'input[name="email"]',
+    signupBtn: 'button[type="submit"]',
+    errorMsg: 'p{style="color: red;"}',
+    successMsg: 'p{style="color: green;"}'
+}
