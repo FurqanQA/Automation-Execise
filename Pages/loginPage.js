b9e0fd88960a71efd77ab76b234d307f18871a06
FurqanMehdi <furqanmehdi831@gmail.com>
2026-08-25 14:41:59 +0500

    Login Successfully

--- Pages/loginPage.js
+++ Pages/loginPage.js
@@ -1,23 +1,38 @@
 import { BasePage } from "./BasePage.js";
 import { loginLocators } from "../Locators/loginLocators.js";
+import { expect } from "@playwright/test";
 
 export class loginPage extends BasePage {
-    constructor(page) {
-        super(page);
-        this.emailInput = page.locator(loginLocators.emailInput);
-        this.passwordInput = page.locator(loginLocators.passwordInput);
-        this.loginBtn = page.locator(loginLocators.loginBtn);
-    }
+  constructor(page) {
+    super(page);
+    this.loginlink = page.locator(loginLocators.loginlink);
+    this.emailInput = page.locator(loginLocators.emailInput);
+    this.passwordInput = page.locator(loginLocators.passwordInput);
+    this.loginBtn = page.locator(loginLocators.loginBtn);
+    this.loggedInUser = page.locator(loginLocators.loggedInUser);
+  }
 
-    async enterEmail(email) {
-        await this.emailInput.fill(email);
-    }
+  async clickLoginLink() {
+    await this.loginlink.click();
+  }
 
-    async enterPassword(password) {
-        await this.passwordInput.fill(password);
-    }   
+  async clickLoginButton() {
+    await this.loginBtn.click();
+  }
 
-    async clickLoginButton() {
-        await this.loginBtn.click();
-    }
-};
+  async enterEmail(email) {
+    await this.emailInput.fill(email);
+  }
+
+  async enterPassword(password) {
+    await this.passwordInput.fill(password);
+  }
+
+  async clickLoginButton() {
+    await this.loginBtn.click();
+  }
+
+  async verifyLoginSuccess() {
+    await expect(this.loggedInUser).toBeVisible();
+  }
+}
--- tests/loginTest.spec.js
+++ tests/loginTest.spec.js
@@ -2,9 +2,16 @@ import { loginPage } from '../Pages/loginPage.js';
 import { test, expect } from '@playwright/test';
 
 test('Login with Valid Credentials', async ({ page }) => {
+
     const login = new loginPage(page);
+
     await login.navigateToHomepage();
-    await login.enterEmail('john.doe@example.com');
-    await login.enterPassword('password123');
+    await login.clickLoginLink();
+    await login.enterEmail('thdevfive@gmail.com');
+    await login.enterPassword('KwbRM@w4UPCEwT');
     await login.clickLoginButton();
+
+    await expect(page).toHaveURL(/automationexercise\.com\/$/);
+
+    await login.verifyLoginSuccess();
 });
