90a49ec469bef72c2eee9f5ef0a1da14b55a12f3
FurqanMehdi <furqanmehdi831@gmail.com>
2026-08-28 16:55:20 +0500

    Test Passed Contact Us

--- Pages/contactusPage.js
+++ Pages/contactusPage.js
@@ -1,51 +1,58 @@
 import { contactusLocators } from "../locators/contactusLocators";
+import { BasePage } from "./basePage";
+import { expect } from "@playwright/test";
 
-export class ContactUsPage {
-    
-    constructor(page) {
-        this.page = page;
-        this.contactUsLink = page.locator(contactusLocators.contactUsLink);
-        this.contactUsHeading = page.locator(contactusLocators.contactUsHeading);
-        
-    }
+export class ContactUsPage extends BasePage {
+  constructor(page) {
+    super(page);
+    this.page = page;
+    this.contactUsLink = page.locator(contactusLocators.contactUsLink);
+    this.contactUsHeading = page.locator(contactusLocators.contactUsHeading);
+    this.uploadFileInput = page.locator(contactusLocators.uploadFileInput);
+    this.successMessage = page.locator(contactusLocators.successMessage);
+    this.errorMessage = page.locator(contactusLocators.errorMessage);
+  }
 
-    async navigateToContactUsPage() {
-        await this.page.goto('https://automationexercise.com/contact_us');
-    }
+  async navigateToContactUsPage() {
+    await this.page.goto("https://automationexercise.com/contact_us");
+  }
 
-    async InputName(name) {
-        await this.page.fill(contactusLocators.nameInput, name);
-    }
+  async InputName(name) {
+    await this.page.fill(contactusLocators.nameInput, name);
+  }
 
-    async InputEmail(email) {
-        await this.page.fill(contactusLocators.emailInput, email);
-    }
+  async InputEmail(email) {
+    await this.page.fill(contactusLocators.emailInput, email);
+  }
 
-    async InputSubject(subject) {
-        await this.page.fill(contactusLocators.subjectInput, subject);
-    }
+  async InputSubject(subject) {
+    await this.page.fill(contactusLocators.subjectInput, subject);
+  }
 
-    async InputMessage(message) {
-        await this.page.fill(contactusLocators.messageInput, message);
-    }
+  async InputMessage(message) {
+    await this.page.fill(contactusLocators.messageInput, message);
+  }
 
-    async UploadFile(filePath) {
-        await this.page.setInputFiles(contactusLocators.uploadFileInput, filePath);
-    }
+  async uploadFile(filePath) {
+    await this.page.setInputFiles(contactusLocators.uploadFileInput, filePath);
+  }
+  async clickSubmitButton() {
+    this.page.once("dialog", async (dialog) => {
+      await dialog.accept();
+    });
 
-    async ClickSubmitButton() {
-        await this.page.click(contactusLocators.submitButton);
-    }
+    await this.page.click(contactusLocators.submitButton);
+  }
 
-    async VerifySuccessMessage() {
-        await this.page.waitForSelector(contactusLocators.successMessage);
-        const successMessage = await this.page.textContent(contactusLocators.successMessage);
-        return successMessage;
-    }
+  async verifySuccessMessage() {
+    await expect(this.successMessage).toBeVisible();
+    await expect(this.successMessage).toHaveText(
+      "Success! Your details have been submitted successfully.",
+    );
+  }
 
-    async VerifyErrorMessage() {
-        await this.page.waitForSelector(contactusLocators.errorMessage);
-        const errorMessage = await this.page.textContent(contactusLocators.errorMessage);
-        return errorMessage;
-    }
+  async verifyErrorMessage() {
+    await expect(this.errorMessage).toBeVisible();
+    await expect(this.errorMessage).toHaveText("Error! Invalid email address.");
+  }
 }
--- Pages/loginPage.js
+++ Pages/loginPage.js
@@ -5,19 +5,17 @@ import { expect } from "@playwright/test";
 export class loginPage extends BasePage {
   constructor(page) {
     super(page);
+
     this.loginlink = page.locator(loginLocators.loginlink);
     this.emailInput = page.locator(loginLocators.emailInput);
     this.passwordInput = page.locator(loginLocators.passwordInput);
     this.loginBtn = page.locator(loginLocators.loginBtn);
     this.loggedInUser = page.locator(loginLocators.loggedInUser);
+    this.logoutBtn = page.locator(loginLocators.logoutBtn);
   }
 
   async clickLoginLink() {
     await this.loginlink.click();
-  }
-
-  async clickLoginButton() {
-    await this.loginBtn.click();
   }
 
   async enterEmail(email) {
@@ -32,11 +30,19 @@ export class loginPage extends BasePage {
     await this.loginBtn.click();
   }
 
+  async loginAsUser(email, password) {
+    await this.clickLoginLink();
+    await this.enterEmail(email);
+    await this.enterPassword(password);
+    await this.clickLoginButton();
+    await this.verifyLoginSuccess();
+  }
+
   async verifyLoginSuccess() {
     await expect(this.loggedInUser).toBeVisible();
   }
 
   async logout() {
-    await this.page.locator(loginLocators.logoutBtn).click();
+    await this.logoutBtn.click();
   }
 }
